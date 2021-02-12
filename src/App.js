ad261757e947c3592ac9c55ec3bdd4486ead5a74
prashant ardeshana <prashant.ardeshana@qrolic.com>
2021-02-12 19:26:43 +0530

    add file with scss setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './style.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar">
+                <div className="logo">
+                    <h1>CloudExpat</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
