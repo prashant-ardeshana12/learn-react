65153957d3bb689698e2e4d6578d79a27a411c9f
prashant ardeshana <prashant.ardeshana@qrolic.com>
2021-02-12 19:38:58 +0530

    successfully install react Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
-import './style.css';
+import './Style.css';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,11 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <div className="navbar">
+            <div className="navbar d-flex">
                 <div className="logo">
                     <h1>CloudExpat</h1>
                 </div>
+                <a href="#" className="btn btn-primary">Hello</a>
             </div>
         </div>
     )
