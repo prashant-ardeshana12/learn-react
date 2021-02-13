090118b41226b8fcdc6ce0fe0bb86e5de936c011
prashant ardeshana <prashant.ardeshana@qrolic.com>
2021-02-13 19:48:40 +0530

    install font awesome and complete header

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <SimpleSlider/>
+      {/* <SimpleSlider/> */}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,71 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
     return (
         <div>
-            <div className="navbar d-flex">
-                <div className="logo">
-                    <h1>CloudExpat</h1>
+            <header>
+                <div className="header">
+                    <div className="header__inner">
+                        <div className="header__logo">
+                            <a href="#">cloudexpat</a>
+                        </div>
+                        <div className="header__menu">
+                            <div className="header__menu-inner">
+                                <ul>
+                                    <li>
+                                        <a href="#">Services</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Partners</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">About Us</a>
+                                    </li>
+                                </ul>
+                                <div className="header__menu-btn">
+                                    <a className="btn-outline" href="#">
+                                        Get in touch
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="toggle-menu">
+                            <i className="far fa-bars"></i>
+                        </div>
+                    </div>
                 </div>
-                <a href="#" className="btn btn-primary">Hello</a>
-            </div>
-        </div>
-    )
-}
+            </header>
 
-export default Header
+            <div className="sidebar">
+                <div className="sidebar__inner">
+                    <div className="sidebar__logo">
+                        <a href="#">cloudexpat</a>
+                    </div>
+                    <div className="sidebar__menu">
+                        <div>
+                            <ul>
+                                <ul>
+                                    <li>
+                                        <a href="#">Services</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Partners</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">About Us</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Get in touch</a>
+                                    </li>
+                                </ul>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="close-sidebar"></div>
+        </div>
+    );
+};
+
+export default Header;
