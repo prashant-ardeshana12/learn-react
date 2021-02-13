62a32d1db081f8a6c183eee171982b115474405e
prashant ardeshana <prashant.ardeshana@qrolic.com>
2021-02-13 18:44:32 +0530

    install jquery and slick slider (add simple slider demo)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // import logo from './logo.svg';
 import './style.scss';
-import Header from './components/Header';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./slick.css"; 
+import "./slick-theme.css";
+
+import Header from './components/Header';
+import SimpleSlider from './components/Slider-demo';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <SimpleSlider/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import $ from "jquery";
 
 ReactDOM.render(
   <React.StrictMode>
